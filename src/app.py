a1f8b876d027d717745cb97030b9dcc4f4eeaf93
Tony Acaldo <tacaldo@gmail.com>
2025-08-04 20:47:24 +0000

    fix: correct typo in project description

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@
 High School Management System API
 
 A super simple FastAPI application that allows students to view and sign up
-for extracurricular activities at Mergington High School.
+for extracurricular activities at Mergington High School..
 """
 
 from fastapi import FastAPI, HTTPException
